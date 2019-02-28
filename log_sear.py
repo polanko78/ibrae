8dc40e7b8708409ad1a3814dc75b59e153bfe361
polanko78 <47469700+polanko78@users.noreply.github.com>
2019-02-28 16:53:12 +0300

    new+

--- log_sear.py
+++ log_sear.py
@@ -1,23 +1,32 @@
+import time
+
+def sort(dict):
+    ip = input('IP? :')
+    filename = ip +'.log'
+    with open(filename, 'w', encoding='UTF-8') as nf:
+        nf.write(ip + '\n')
+        if ip in dict.keys():
+            for x in dict[ip]:
+                nf.write('Дата: {}  Время в работе: {}  Объем данных: {}  URL : {}'.format(time.ctime(float(x['date'])), x['time'], x['size'], x['url'] + '\n'))
+#                print('Дата: {}  Время в работе: {}  Объем данных: {}  URL : {}'.format(time.ctime(float(x['date'])), x['time'], x['size'], x['url']))
 
 dict = {}
-with open(test.log) as f:
-    while True:
+x = True
+with open('test.log') as f:
+    while x == True:
         ip_line = []
         list_tmp = f.readline()
         if list_tmp is not '':
             list = list_tmp.split(' ')
             list[:] = [x for x in list if x != '']
-            date = list[0]
-            time = list[1]
             ip = list[2]
-            size = list[4]
-            url = list[6]
-            session_line = {'date': date, 'time': time, 'size': size, 'url': url}
+            session_line = {'date': list[0], 'time': list[1], 'size': list[4], 'url': list[6]}
             if ip in dict.keys():
                 dict[ip].append(session_line)
-                print(dict[ip])
             else:
                 ip_line.append(session_line)
                 dict[ip] = ip_line
         else:
-            return self
+            x = False
+print(dict.keys())
+sort(dict)
